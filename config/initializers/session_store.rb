cae43d29a274c6b3eafcab6d0449a44a084fbddf
shiro615 <ys2014hs@gmail.com>
2015-01-04 20:52:22 +0900

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FutsalApp::Application.config.session_store :cookie_store, key: '_futsal_app_session'
---
+++ vendor/bundle/ruby/2.0.0/gems/mysql2-0.3.17/ext/mysql2/siteconf20150104-97701-3h9716.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150104-97701-de7b66"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
