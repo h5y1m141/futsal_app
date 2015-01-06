94196c8242195b3872ab8d7a9ae7720fed286e72
HiroshiOyamada <h5y1m141@gmail.com>
2015-01-07 08:45:25 +0900

    フットサルコートの情報が格納できるようにplaceモデルを準備

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105124704) do
+ActiveRecord::Schema.define(version: 20150106232839) do
 
   create_table "maps", force: true do |t|
     t.string   "title"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20150105124704) do
 
   create_table "ochiais", force: true do |t|
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "places", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.integer  "price"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
